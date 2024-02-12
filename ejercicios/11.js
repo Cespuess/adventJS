964f3d99dae5b2ff4448fa0f2744fe1cb4f1c0a5
Carles Espuñes <cespuess@gmail.com>
2024-02-12 17:18:56 +0100

    Pasan todos los test de AdventJS menos "rotaratov"

---
+++ ejercicios/11.js
@@ -0,0 +1,97 @@
+function getIndexsForPalindrome(word) {
+  /*
+  En el taller de Santa, los elfos aman los acertijos 🧠. Este año, han creado uno especial: un desafío para formar un palíndromo navideño.
+
+  Un palíndromo es una palabra que se lee igual hacia adelante y hacia atrás. Los elfos quieren saber si es posible formar un palíndromo haciendo, como mucho, un intercambio de letras.
+
+  Crea una función getIndexsForPalindrome que reciba una cadena de caracteres y devolverá:
+
+  Si ya es un palíndromo, un array vacío.
+  Si no es posible, null.
+  Si se puede formar un palíndromo con un cambio, un array con las dos posiciones (índices) que se deben intercambiar para poder crearlo.
+  Por ejemplo:
+
+  getIndexsForPalindrome('anna') // []
+  getIndexsForPalindrome('abab') // [0, 1]
+  getIndexsForPalindrome('abac') // null
+  getIndexsForPalindrome('aaaaaaaa') // []
+  getIndexsForPalindrome('aaababa') // [1, 3]
+  getIndexsForPalindrome('caababa') // null
+  Si se puede formar el palíndromo con diferentes intercambios, siempre se debe devolver el primero que se encuentre.
+  */
+
+  let res = [];
+
+  if (!isPalindrome(word)) {
+    res = changeIndex(word)
+  }
+
+  return res;
+}
+
+function isPalindrome(word) {
+  // Recibe una palabra y devuelve true o false dependiendo de si es palíndromo 
+  let i = 0;
+  let f = word.length - 1;
+
+  while (i <= f) {
+    if (word[i] !== word[f]) return false;
+    i++;
+    f--;
+  }
+
+  return true;
+}
+
+function changeIndex(word) {
+  //recibe la palabra no palíndroma y cambia la letra para ver si con un cambio puede ser palíndroma.
+  let i = 0;
+  let f = word.length - 1;
+
+  while (i <= f) {
+    if (word[i] !== word[f]) {
+      let indexF = findIndex(word, word[f], i);
+
+      for (let j = 0; j < findIndex.length; j++) {
+        let newWord = changeLetter(word, i, indexF[j]);
+        if (isPalindrome(newWord)) return [i, indexF[j]];
+      }
+    }
+    
+
+    i++;
+    f--;
+  }
+
+  return null
+}
+
+function changeLetter(word, index1, index2) {
+  // convierte la palabra en array para cambiar los caracteres de sitio
+  let wordArray = word.split('');
+
+  let temp = wordArray[index1];
+  wordArray[index1] = wordArray[index2];
+  wordArray[index2] = temp;
+
+  let changedWord = wordArray.join('');
+
+  return changedWord;
+
+}
+
+function findIndex(word, character, i) {
+  // devuelve una array con los índices de donde se encuentra el carácter, si no hay coincidencias devuelve array vacío.
+  let indexArray = [];
+  let index = word.indexOf(character, i);
+
+  while (index !== -1) {
+    indexArray.push(index);
+    index = word.indexOf(character, index + 1);
+  }
+
+  return indexArray;
+}
+
+
+module.exports = {getIndexsForPalindrome, isPalindrome, changeLetter, changeIndex, findIndex}
---
+++ test/11.test.js
@@ -0,0 +1,47 @@
+const {getIndexsForPalindrome} = require('../ejercicios/11');
+const {isPalindrome} = require('../ejercicios/11');
+const {changeIndex} = require('../ejercicios/11');
+const {changeLetter} = require('../ejercicios/11');
+const {findIndex} = require('../ejercicios/11');
+
+describe('palíndromos con cambios', () => {
+  test('true si es palíndromo', () => {
+    expect(isPalindrome('anna')).toBeTruthy();
+    expect(isPalindrome('carac')).toBeTruthy();
+  });
+  test('false si no es paíndromo', () => {
+    expect(isPalindrome('abac')).toBeFalsy();
+    expect(isPalindrome('carles')).toBeFalsy();
+  });
+  test('encuentra los índices de la letra pedida', () => {
+    expect(findIndex('banana', 'a', 0)).toEqual([1,3,5]);
+    expect(findIndex('susanas', 's', 2)).toEqual([2,6]);
+    expect(findIndex('furgoneta', 'c')).toEqual([]);
+  })
+  test('palabras nuevas con letras cambiadas', () => {
+    expect(changeLetter('carles', 1, 3)).toBe('clraes');
+    expect(changeLetter('leona', 3,4)).toBe('leoan');
+  });
+  test('index de letras a cambiar para que sea palíndromo', () => {
+    expect(changeIndex('abab')).toEqual([0,1]);
+    expect(changeIndex('aaababa')).toEqual([1,3]);
+    expect(changeIndex('anerera')).toEqual([1, 3]);
+
+  });
+  test('null si ni cambiando letras es palíndromo', () => {
+    expect(changeIndex('caababa')).toBeNull();
+    expect(changeIndex('carles')).toBeNull();
+  });
+  test('devuelve [] si es palíndroma inicialmente', () => {
+    expect(getIndexsForPalindrome('anna')).toEqual([]);
+    expect(getIndexsForPalindrome('reconocer')).toEqual([]);
+  });
+  test('devuelve los índices de los cambios para que sea palíndroma', () => {
+    expect(getIndexsForPalindrome('aaababa')).toEqual([1, 3]);
+    expect(getIndexsForPalindrome('anerera')).toEqual([1, 3]);
+  });
+  test('null si ni con cambios en palíndroma', () => {
+    expect(getIndexsForPalindrome('caababa')).toBeNull();
+    expect(getIndexsForPalindrome('carles')).toBeNull();
+  })
+})
